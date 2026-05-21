e2629c7659ae22b22627dfe13c85b81f3d7e96e2
dd-jiyun <dlwldbs8015@gmail.com>
2026-05-21 23:11:10 +0900

    initial setting

---
+++ src/main/java/com/liveklass/LiveKlassTestApplication.java
@@ -0,0 +1,13 @@
+package com.liveklass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveKlassTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveKlassTestApplication.class, args);
+    }
+
+}
