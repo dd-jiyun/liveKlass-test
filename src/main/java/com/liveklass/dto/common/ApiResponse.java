68a0bcfa74318d5172aad25fc7861b1e3f3be39f
dd-jiyun <dlwldbs8015@gmail.com>
2026-05-24 21:10:41 +0900

    feat: ApiHeaders 및 ApiResponse 정의

---
+++ src/main/java/com/liveklass/dto/common/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.liveklass.dto.common;
+
+public record ApiResponse<T>(T data) {
+
+    public static <T> ApiResponse<T> of(T data) {
+        return new ApiResponse<>(data);
+    }
+}
+
---
+++ src/main/java/com/liveklass/global/constant/ApiHeaders.java
@@ -0,0 +1,9 @@
+package com.liveklass.global.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApiHeaders {
+    public static final String USER_ID = "X-User-Id";
+}
